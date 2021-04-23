038d4f607b73fede5c248a2a9e1052affdc1cce9
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-23 20:45:33 +0600

    Controllers Initialized

---
+++ BlogApiWithASPNetCore/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BlogApiWithASPNetCore.DataAccess.Repositories.IRepositories;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApiWithASPNetCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUnitOfWork _db;
+
+        public UsersController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+    }
+}
